3fe08ce432c6fc2792685b445a79860fd14862d6
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-17 02:31:44 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -16,7 +17,8 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
+  
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
@@ -55,6 +60,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][0]
+     
 end
 
 def adding_matz
@@ -80,6 +88,12 @@ def adding_matz
         }
      }
 
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby", 
+    :languages => ["LISP", "C"]
+  }
+  
+  programmer_hash
     
 end
 
@@ -100,8 +114,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+  alans_new_info = "GUI"
      
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
+  programmer_hash
      
 end
 
@@ -121,5 +138,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
+  
 end
